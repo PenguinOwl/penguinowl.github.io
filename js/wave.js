1d5818c2156da5eff30d265c324172bbef12f68c
Henry <henryjboles@gmail.com>
2017-03-16 21:18:49 -0700

    Delete wave.js

--- js/wave.js
+++
@@ -1,9 +0,0 @@
-var text = "Welcome";
-
-for(var i in text) { 
-  if(text[i] === " ") {
-    $(".wavetext").append( $("<span>").html("&nbsp;") ); 
-  } else {  
-    $(".wavetext").append( $("<span>").text(text[i]) ); 
-  }
-}
