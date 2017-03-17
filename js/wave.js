a82f5cdb77796c0abb36118020dc722121140223
Henry <henryjboles@gmail.com>
2017-03-16 20:40:57 -0700

    Create wave.js

---
+++ js/wave.js
@@ -0,0 +1,9 @@
+var text = "Welcome";
+
+for(var i in text) { 
+  if(text[i] === " ") {
+    $(".wavetext").append( $("<span>").html("&nbsp;") ); 
+  } else {  
+    $(".wavetext").append( $("<span>").text(text[i]) ); 
+  }
+}
